dcdc51b6b30ade3ddf9eb58c02b30e122a2592ac
Fiona Sterling <fsterling@hchc.edu>
2018-10-03 14:13:12 -0400

    Rendering products, TODO: functionality for add to cart button

--- Desktop/Coding Temple Python Bootcamp/Week 7/gemstore/src/components/views/home/home.js
+++ Desktop/Coding Temple Python Bootcamp/Week 7/gemstore/src/components/views/home/home.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './home.css';
+import ProductList from '../../productList/productList.js';
 
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h1>Home</h1>
-      </div>
+        <div className="container">
+          <ProductList products={this.props.products} />
+        </div> {/*ends container*/}
+      </div> //ends div for Home
     );
   }
 }
