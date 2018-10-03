f292a9f9c326ff6479319ca806609f561ab78e20
Fiona Sterling <fsterling@hchc.edu>
2018-10-03 11:19:22 -0400

    Navbar completed, TODO: create proper links.

--- Desktop/Coding Temple Python Bootcamp/Week 7/gemstore/src/App.js
+++ Desktop/Coding Temple Python Bootcamp/Week 7/gemstore/src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import products from './static/data/products.js';
+import Navbar from './components/navbar/navbar.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      total: undefined,
+      cart: undefined,
+      products: undefined
+    }
+  }
+
+  componentWillMount() {
+    this.setState({
+      total : 0,
+      cart: [],
+      products: products
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Navbar />
       </div>
     );
   }
